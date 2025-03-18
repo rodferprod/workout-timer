0eb95ce88b5060c435740e676d95557700366f08
RodrigoJGF <rodrigoferreira.tico@gmail.com>
2025-03-18 15:49:31 -0300

    feat: Changing the duration workout manually

--- src/Calculator.js
+++ src/Calculator.js
@@ -1,4 +1,4 @@
-import { memo, useState } from "react";
+import { memo, useEffect, useState } from "react";
 import clickSound from "./ClickSound.m4a";
 
 function Calculator({ workouts, allowSound }) {
@@ -6,8 +6,22 @@ function Calculator({ workouts, allowSound }) {
 	const [sets, setSets] = useState(3);
 	const [speed, setSpeed] = useState(90);
 	const [durationBreak, setDurationBreak] = useState(5);
+	const [duration, setDuration] = useState(0);
 
-	const duration = (number * sets * speed) / 60 + (sets - 1) * durationBreak;
+	useEffect(() => {
+		setDuration((number * sets * speed) / 60 + (sets - 1) * durationBreak);
+	}, [number, sets, speed, durationBreak]);
+
+	const increaseDuration = () => {
+		setDuration((actualDuration) => actualDuration + 1);
+	};
+
+	const decreaseDuration = () => {
+		setDuration((actualDuration) =>
+			actualDuration > 1 ? actualDuration - 1 : 0
+		);
+	};
+
 	const mins = Math.floor(duration);
 	const seconds = (duration - mins) * 60;
 
@@ -73,13 +87,13 @@ function Calculator({ workouts, allowSound }) {
 				</div>
 			</form>
 			<section>
-				<button onClick={() => {}}>–</button>
+				<button onClick={decreaseDuration}>–</button>
 				<p>
 					{mins < 10 && "0"}
 					{mins}:{seconds < 10 && "0"}
 					{seconds}
 				</p>
-				<button onClick={() => {}}>+</button>
+				<button onClick={increaseDuration}>+</button>
 			</section>
 		</>
 	);
